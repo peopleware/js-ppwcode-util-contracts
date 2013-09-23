4bef39d3c631af33d704f14f6a1d96acefcbf24d
Mark Cuypers <Mark_Cuypers@peopleware.be>
2013-09-23 09:29:30 +0000

    Updated the package.json file for ppwcode.
    git-svn-id: https://ppwcode.googlecode.com/svn/javascript/util/contracts/I/1.n/trunk@7090 463e38ab-4f4e-0410-907a-65d2b3888964

--- src/contracts/ppwcode_contracts.profile.js
+++ src/contracts/ppwcode_contracts.profile.js
@@ -1,16 +1,28 @@
 var profile = (function() {
+  function isTest(filename, mid) {
+    return filename.indexOf("test/") >= 0 || filename.indexOf("doh") >= 0;
+  }
+
+  function isCopyOnly(filename, mid) {
+    return filename.indexOf(".html") >= 0;
+  }
+
+  function isAmd(filename, mid) {
+    return filename.indexOf(".json") < 0 && filename.indexOf(".js") >= 0 && filename.indexOf(".profile.js") < 0;
+  }
+
   return {
     resourceTags: {
-      test: function(filename, mid) {
-        return filename == "doh.js";
+      test: function (filename, mid) {
+        return isTest(filename, mid);
       },
 
-      copyOnly: function(filename, mid) {
-        return false;
+      copyOnly: function (filename, mid) {
+        return isCopyOnly(filename, mid);
       },
 
-      amd: function(filename, mid) {
-        return filename != "doh.js" && /\.js$/.test(filename);
+      amd: function (filename, mid) {
+        return !isCopyOnly(filename, mid) && !isTest(filename, mid) && isAmd(filename, mid);
       }
     }
   };
