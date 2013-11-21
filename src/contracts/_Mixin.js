d689bf331cd3b6e1f1a3bf3ff1dbafc7078aac9b
Jan Dockx <Jan_Dockx@peopleware.be>
2013-11-21 09:53:08 +0000

    prepared for release
    git-svn-id: https://ppwcode.googlecode.com/svn/javascript/util/contracts/I/1.n/trunk@7261 463e38ab-4f4e-0410-907a-65d2b3888964

--- src/contracts/_Mixin.js
+++ src/contracts/_Mixin.js
@@ -1,3 +1,19 @@
+/*
+ Copyright 2012 - $Date $ by PeopleWare n.v.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 define(["dojo/_base/declare", "dojo/_base/lang", "ppwcode-util-oddsAndEnds/js", "dojo/has"],
   function(declare, lang, js, has) {
 
@@ -25,10 +41,8 @@ define(["dojo/_base/declare", "dojo/_base/lang", "ppwcode-util-oddsAndEnds/js", 
       _c_invar: [],
 
       _c_pre: function(condition) {
-        if (has("ppwcode-contracts-precondition")) {
-          if (! condition.apply(this)) {
-            throw new _PreconditionViolationError(this, this._c_pre.caller, this._c_pre.caller.caller, this._c_pre.caller.arguments, condition);
-          }
+        if (has("ppwcode-contracts-precondition") && !condition.apply(this)) {
+          throw new _PreconditionViolationError(this, this._c_pre.caller, this._c_pre.caller.caller, this._c_pre.caller.arguments, condition);
         }
       },
 
--- src/contracts/doh.js
+++ src/contracts/doh.js
@@ -1,3 +1,19 @@
+/*
+ Copyright 2012 - $Date $ by PeopleWare n.v.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 define(["dojo/_base/declare", "doh/main", "dojo/_base/lang"],
   function(declare, doh, lang) {
 
@@ -55,7 +71,7 @@ define(["dojo/_base/declare", "doh/main", "dojo/_base/lang"],
     };
 
     doh.validateInvariants = function(subject) {
-      // TODO subject is a _Mixin (duck typing)
+      // subject is a _Mixin IDEA check with duck typing
       doh.isNot(null, subject);
       doh.t("_c_invar" in subject);
       doh.isNot(null, subject._c_invar);
