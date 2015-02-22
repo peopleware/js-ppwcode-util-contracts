771209133c2ccfccd40f8116d1c59b851bdcd3c9
Jan Dockx <Jan_Dockx@peopleware.be>
2015-02-22 22:17:58 +0100

    Code reformat

--- _Mixin.js
+++ _Mixin.js
@@ -1,18 +1,18 @@
 /*
- Copyright 2012 - $Date $ by PeopleWare n.v.
+  Copyright 2012 - $Date $ by PeopleWare n.v.
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
 
- http://www.apache.org/licenses/LICENSE-2.0
+  http://www.apache.org/licenses/LICENSE-2.0
 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- */
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
 
 define(
   ["dojo/_base/declare", "dojo/_base/lang", "ppwcode-util-oddsAndEnds/js", "dojo/has"],
@@ -45,9 +45,13 @@ define(
 
         _c_pre: function(condition) {
           if (has("ppwcode-contracts-precondition") && !condition.apply(this)) {
-            throw new _PreconditionViolationError(this, this._c_pre.caller,
-                                                  this._c_pre.caller.caller,
-                                                  this._c_pre.caller.arguments, condition);
+            throw new _PreconditionViolationError(
+              this,
+              this._c_pre.caller,
+              this._c_pre.caller.caller,
+              this._c_pre.caller.arguments,
+              condition
+            );
           }
         },
 
